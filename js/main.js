53fbe3ec8b248529ca956198c1ce1b89d7527a37
Mark Saiget <saiget@gmail.com>
2016-04-13 00:21:09 -0700

    Update js to use mapbox-gl via browserify-shim

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-'use strict';
-var mapboxgl = require('mapbox-gl');
+var mapgl = require('mapbox-gl');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFya2tvbmciLCJhIjoiY2lteWNjMzFhMDQzbXZvbHUza3B6eTdoaSJ9.HuonodjO41vjRpMJk29lXA';
 var map = new mapboxgl.Map({
